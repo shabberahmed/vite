c6ca3ef7c16b8e57db7695e86b41a72424263737
AHMED SHAIK <ahmed@AHMEDs-MacBook-Pro.local>
2023-10-28 12:22:55 +0530

    oo

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,9 @@
-import path from 'path';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import checker from 'vite-plugin-checker';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import react from '@vitejs/plugin-react-swc';
 
-// ----------------------------------------------------------------------
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1'),
-      },
-    ],
+  build: {
+    outDir: 'dist', // Customize the publish directory here
   },
-  server: {
-    hmr: {
-      overlay: false,
-    },
-    port: 3030,
-  },
-  preview: {
-    port: 3030,
-  },
+  // Other Vite configuration options can be added here as needed
 });
